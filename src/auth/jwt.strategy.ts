e77e49c02e0d314701b52ab2d221eb4d93d30832
async-cat0 <sasazerdev0@gmail.com>
2026-05-13 09:36:48 +0300

    feat (Auth):  finalized registration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,20 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
-import { Request } from "@nestjs/common";
+import { Injectable, Request } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    //constructor() {
-        // super({
-            //jwtFromRequest: (req: Request) => req?.cookie?.access_token,
-           // ignoreExpiration: false,
-            //secretOrKey: process.env.JWT_SECRET,
-        //});
-    //}
+  constructor() {
+    super({
+      jwtFromRequest: (req: Request) => req?.cookie?.access_token,
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
 
-    // payload - расскодированный jwt
-    validate(payload: {sub: number; email: string}) {
-        return {userId: payload.sub, email: payload.email}
-    }
+  //payload - расскодированный jwt
+  validate(payload: { sub: number; email: string }) {
+    return { userId: payload.sub, email: payload.email };
+  }
 }
